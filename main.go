5a1491b77b26882f65f9699bf63be05381363eed
mfernandez <matiasef90@gmail.com>
2025-08-05 17:47:28 +0000

    Created healthz endpoint

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+		body := []byte("OK")
+		w.Write(body)
+		w.WriteHeader(http.StatusOK)
+	})
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/assets", http.FileServer(http.Dir("assets/logo.png")))
 	server := http.Server{}
 	server.Handler = mux
 	server.Addr = ":8080"
